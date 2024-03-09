4efe692cfe5f1ed7fb8d0954646aea21a1e6b432
Gabriel YR <83891241+GaboYR@users.noreply.github.com>
2024-03-09 18:11:24 -0500

    Update A.cpp

--- BinarySearch/A.cpp
+++ BinarySearch/A.cpp
@@ -1,30 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define all(x) x.begin(),x.end()
-#define sz(x) (int)x.size()
-#define fast_io ios_base::sync_with_stdio(false);cin.tie(nullptr);
-#define cin std::cin
-#define cout std::cout
-using ll = long long;
-using ld = long double;
-using ii = pair<int,int>;
-using vi = vector<int>;
-using vii = vector<ii>;
-int main(){
-    fast_io
-    int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
-    int Sz; cin >> Sz;
-    string s;   cin >> s;
-    x1 -= x2;
-    y1 -= y2;
-    char dx,dy;
-    if (x1 > 0) dx = 'R';
-    else if (x1 < 0)    dx = 'L';
-    if (y2 > 0) dy = 'U';
-    else if (y2 < 0)    dy = 'D';
-    
-    
-    return 0;
+int x,y,p,q,len;
+string s;
+int xx = 0,yy = 0;
+bool f(long long m)
+{
+    long long d = m / len;
+    long long r = m % len;
+    long long dx = xx * d;
+    long long dy = yy * d;
+    for (int i = 0; i < r; i ++)
+    {
+        if (s[i] == 'U')    dy ++;
+        if (s[i] == 'D')    dy --;
+        if (s[i] == 'L')    dx --;
+        if (s[i] == 'R')    dx ++;
+    }
+    return abs(p - (x + dx)) + abs(q - (y + dy)) <= m;
+}
+int main ()
+{
+    cin >> x >> y >> p >> q >> len;
+    cin >> s;
+    for (int i = 0; i < len; i ++)
+    {
+        if (s[i] == 'U')    yy ++;
+        if (s[i] == 'D')    yy --;
+        if (s[i] == 'L')    xx --;
+        if (s[i] == 'R')    xx ++;
+    }
+    long long low = 0, high = 4e18 + 5;
+    while (high - low != 1)
+    {
+        //cout << low << ' ' << high << '\n';
+        long long mid = low + (high - low) / 2;
+        if (f(mid)) high = mid;
+        else low = mid;
+    }
+    //cout << low << ' ' << high << '\n';
+    cout << (f(high)?high : - 1) << '\n';
 }
 //Para lectura o salida de archivo
 //g++ name.cpp  < in.txt(lee los datos desde el archivo in.txt)
