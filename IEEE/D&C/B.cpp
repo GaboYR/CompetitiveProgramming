0d14fc7820c238318e1107858901f1cb30fba9ed
Gabriel YR <83891241+GaboYR@users.noreply.github.com>
2024-03-23 17:41:32 -0500

    Update B.cpp

--- IEEE/D&C/B.cpp
+++ IEEE/D&C/B.cpp
@@ -1,53 +1,59 @@
+/*
+    https://codeforces.com/group/OyefIGO3t4/contest/447261/problem/B
+    https://codeforces.com/problemset/problem/459/D
+/*
 #include<bits/stdc++.h>
 using namespace std;
-#define all(x) x.begin(),x.end()
 #define sz(x) (int)x.size()
-#define fast_io ios_base::sync_with_stdio(false);cin.tie(nullptr);
-#define cin std::cin
-#define cout std::cout
-using ll = long long;
-using ld = long double;
-using ii = pair<int,int>;
-using vi = vector<int>;
-using vii = vector<ii>;
 map<int,int> p,q;
-ll cont = 0;
-ll res = 0;
-ll merge(vi &L,vi &R) {
+long long dc(vector<int> &l,vector<int> &r)
+{
+    long long ans = 0;
     int i = 0,j = 0;
-    while (i < sz(L) and j < sz(R)) {
-        if (L[i] > R[j]) {
-
+    while (i < sz(l) && j < sz(r))
+    {
+        if (r[j] < l[i])    j ++;
+        else 
+        {
+            i ++;
+            ans += j;
         }
     }
+    while (i ++ < sz(l))   ans += j;
+	return ans;
 }
-ll solve(vi &v) {
-    if (sz(v) == 1) return 0;
-    int mid = sz(v)/2;
-    vi L(v.begin(),v.begin() + mid);
-    vi R(v.begin() + mid,v.end());
-    res += solve(L) + solve(R);
-    return res + merge(L,R);
+long long merge(vector<int> &a,vector<int> &b)
+{
+    if (sz(a) <= 1)  return 0;
+    int mid = sz(a) / 2;
+
+    vector<int> la(a.begin(),a.begin() + mid);
+    vector<int> ra(a.begin() + mid,a.end());
+    vector<int> lb(b.begin(),b.begin() + mid);
+    vector<int> rb(b.begin() + mid,b.end());
+
+    long long res = merge(la,lb) + merge(ra,rb);
+
+    sort(la.begin(),la.end());
+    sort(rb.begin(),rb.end());
+
+    return res + dc(la,rb);
 }
-int main(){
-    fast_io
+int main ()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
     int n;  cin >> n;
-    vi v(n),contL(n),contR(n);
-    for (int i = 0; i < n; i ++)    cin >> v[i];
-    for (int i = 0; i < n; i ++) {
-        contL[i] = ++ p[v[i]];
-        contR[n - i - 1] = ++ q[v[n - i - 1]];
+    vector<int> a(n),contL(n),contR(n);
+    
+    for (int i = 0; i < n; i ++)   cin >> a[i];
+    
+    for (int i = 0; i < n; i ++)
+    {
+        contL[i] = ++ p[a[i]];
+        contR[n - i - 1] = ++ q[a[n - i - 1]];
     }
-    ll ans = 0;
-    // for (int i = 0; i < n; i ++) {
-    //     if (contL[i] == 1)  continue;
-    //     else {
-    //         for (int j = n - 1; j > i;j --) {
-    //             if (contR[j] < contL[i])    ans ++;
-    //         }
-    //     }
-    // }
-    res = solve(contL,contR);
-    cout << res - exc<< "\n";
+    cout << merge(contL,contR) << '\n';
     return 0;
 }
